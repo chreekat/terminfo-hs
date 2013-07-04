82a63a6b4eb11eb523948a747e6c3ae16696b7fb
Bryan Richter <bryan.richter@gmail.com>
2013-07-04 14:34:56 +0300

    Rename API elements and add some docs.

--- Terminfo.hs
+++ Terminfo.hs
@@ -17,13 +17,16 @@ module Terminfo (
     , TIDatabase
     -- * Querying Capabilities
     -- $queryFuncs
-    , queryBoolCapability
-    , queryNumCapability
-    , queryStrCapability
-    -- ** The Capabalities
-    , TermBoolCapability(..)
-    , TermNumCapability(..)
-    , TermStrCapability(..)
+    , queryBoolTermCap
+    , queryNumTermCap
+    , queryStrTermCap
+    -- ** The Capabilities
+    , BoolTermCap(..)
+    , NumTermCap(..)
+    , StrTermCap(..)
+    -- * Rationale
+    -- ** TIDatabase
+    -- $rationaleTID
     )
 where
 
@@ -33,9 +36,9 @@ import Data.Text
 
 data TIDatabase = TIDatabase
 
-data TermBoolCapability = TermBoolCapability
-data TermNumCapability = TermNumCapability
-data TermStrCapability = TermStrCapability
+data BoolTermCap = BoolTermCap
+data NumTermCap = NumTermCap
+data StrTermCap = StrTermCap
 
 acquireDatabase
     :: Text -- ^ Terminal name
@@ -45,22 +48,41 @@ acquireDatabase term = $(todo "acquireDatabase()")
 
 -- | This action wraps both nonexistent and false-valued capabilities into
 -- a return value of 'False'.
-queryBoolCapability :: TIDatabase
-                    -> TermBoolCapability
-                    -> IO Bool
-queryBoolCapability = $notImplemented
+queryBoolTermCap :: TIDatabase
+                 -> BoolTermCap
+                 -> Bool
+queryBoolTermCap = $notImplemented
 
-queryNumCapability :: TIDatabase
-                   -> TermNumCapability
-                   -> IO (Maybe Int)
-queryNumCapability = $notImplemented
+queryNumTermCap :: TIDatabase
+                -> NumTermCap
+                -> Maybe Int
+queryNumTermCap = $notImplemented
 
-queryStrCapability :: TIDatabase
-                   -> TermStrCapability
-                   -> IO (Maybe Text)
-queryStrCapability = $notImplemented
+-- | As this is a dead simple module, no 'smart' handling of the returned
+-- string is implemented. In particular, placeholders for buffer characters
+-- and command arguments are left as-is. This will be rectified eventually,
+-- probably in a separate module.
+queryStrTermCap :: TIDatabase
+                -> StrTermCap
+                -> Maybe Text
+queryStrTermCap = $notImplemented
 
--- $queryFuncs I'm not super proud of this interface, but it's the best I
--- can manage at present. For each of these three functions, the first
--- argument is the database to query, and the second argument is the
--- capability to look up.
+-- $queryFuncs
+--
+-- For each of these three actions, the first argument is the database to
+-- query, and the second argument is the capability to look up.
+--
+-- I'm not super proud of this interface, but it's the best I can manage at
+-- present without requiring the user to use lots of mostly-empty case
+-- expressions. Perhaps someone will suggest a more interesting solution.
+
+-- $rationaleTID
+--
+-- One could imagine a simpler interface that hides the TIDatabase type
+-- entirely. This could be done naively by rolling 'acquireDatabase' into
+-- the query actions, or more \'intelligently\' by creating some
+-- 'State'-based monad instance. But the first would read the db file on
+-- every query, which strikes me as entirely /too/ naive, and the second
+-- would force the user to add Yet Another Monad to their application's
+-- stack. Unless somebody has a better idea, I think that exposing
+-- TIDatabase strikes the best balance.
